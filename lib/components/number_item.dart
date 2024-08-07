e55177391501147c58be164440f777cb9909fba6
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-08-07 15:46:21 +0300

    add plugins

--- lib/components/number_item.dart
+++ lib/components/number_item.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tocu/models/number.dart';
 
-
 class NumberItem extends StatelessWidget {
-  const NumberItem ({Key? key, required this.number}) : super(key : key);
+  const NumberItem({Key? key, required this.number}) : super(key: key);
   final Number number;
   @override
   Widget build(BuildContext context) {
@@ -12,26 +11,19 @@ class NumberItem extends StatelessWidget {
       color: Color(0xff6482AD),
       child: Row(
         children: [
-          Container(
-              color: Color(0xa8193257),
-              child: Image.asset(number.image)
-          ),
+          Container(color: Color(0xa8193257), child: Image.asset(number.image)),
           Padding(
             padding: const EdgeInsets.only(left: 16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(number.jpName,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18
-                  ),
+                Text(
+                  number.jpName,
+                  style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
-                Text(number.enName,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18
-                  ),
+                Text(
+                  number.enName,
+                  style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
               ],
             ),
@@ -39,9 +31,13 @@ class NumberItem extends StatelessWidget {
           Spacer(
             flex: 1,
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Icon(
+
+          IconButton(
+            splashColor: Colors.black,
+            onPressed: (){
+              // //   play sound
+            },
+            icon: Icon(
               Icons.play_circle,
               color: Colors.white,
               size: 32,
